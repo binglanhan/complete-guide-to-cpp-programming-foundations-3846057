7e94f83a8ca5294a7dc1401f239fca01a94e62cf
binglanhan <105531152+binglanhan@users.noreply.github.com>
2025-03-10 02:32:00 +0000

    added a comment

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03 wiht modifications
+// Exercise 00_03 wiht modifications New 2025
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
